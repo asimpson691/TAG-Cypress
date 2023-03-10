11ea58cdfecf1a77f6ecad400643c6cfbb5f7e64
Rosie Atkins <rosie.atkins@raytheon.co.uk>
2023-03-10 12:48:17 +0000

    deleted duplicate cypress.congig file

--- todomvc-react/cypress.config.js
+++
@@ -1,9 +0,0 @@
-// Original 
-// module.exports = {
-//     baseUrl: 'https://todomvc.com/examples/react/#/'
-//   }
-  
-// Local hosted
-module.exports = {
-    baseUrl: 'http://127.0.0.1:8080'
-  }
