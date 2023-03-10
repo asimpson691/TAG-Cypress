29259cea6a34dcc85fa6b997b1ca8e3b16f4de71
Rosie Atkins <rosie.atkins@raytheon.co.uk>
2023-03-10 12:02:56 +0000

    saved update file to re-format with prettier extention

--- cypress/e2e/steps/todos/update.js
+++ cypress/e2e/steps/todos/update.js
@@ -4,15 +4,18 @@ Updates the specified todo items
 @param {string[]} items The items to create
 */
 export function markTodoAsDone(items) {
-items.forEach((item) => {
-cy.get('.todo-list').within(() => {
-cy.get("label").contains(item).parent()
-.get('input[class="toggle"]').click({ multiple: true })
-})
-})
+	items.forEach((item) => {
+		cy.get(".todo-list").within(() => {
+			cy.get("label")
+				.contains(item)
+				.parent()
+				.get('input[class="toggle"]')
+				.click({ multiple: true });
+		});
+	});
 }
 export function markAllTodosAsDone() {
-cy.get('.todo-list').within(() => {
-cy.get('input[class="toggle"]').click({ multiple: true })
-})
+	cy.get(".todo-list").within(() => {
+		cy.get('input[class="toggle"]').click({ multiple: true });
+	});
 }
